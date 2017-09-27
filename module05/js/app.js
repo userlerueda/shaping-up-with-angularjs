e7f0aa020bd2cd31ba9dc18bbc0a46ebfd969cda
Luis Rueda <userlerueda@gmail.com>
2017-09-26 23:49:03 -0500

    Adds module #5

---
+++ module05/js/app.js
@@ -0,0 +1,33 @@
+(function() {
+  var app = angular.module('gemStore', ['store-directives']);
+
+  app.controller('StoreController', ['$http', function($http) {
+    var store = this;
+    store.products = [];
+
+    $http.get('/module05/store-products.json')
+    .then(successCallback, errorCallback);
+
+    function successCallback(response){
+      store.products = response.data;
+    }
+
+    function errorCallback(error){
+      console.log(error);
+    }
+
+  }]);
+
+  app.controller("ReviewController", function(){
+
+    this.review = {};
+
+    this.addReview = function(product){
+      this.review.createdOn = Date.now();
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
+
+
+})();
